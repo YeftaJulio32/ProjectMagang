fd040257c809806ac2df424777b5b57c80088b9d
FebriTPP <febritripputra14@gmail.com>
2025-06-11 22:59:49 +0700

    minor change

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,13 +8,23 @@
   </form>
 
   @auth
-    <span class="navbar-text text-light me-3">
-      Selamat datang, {{ auth()->user()->name }}!
-    </span>
-    <form method="POST" action="{{ route('logout') }}" class="d-inline">
-      @csrf
-      <button type="submit" class="btn btn-outline-light">Logout</button>
-    </form>
+    <div class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle text-light" href="#" id="navbarUserDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-person-circle me-1"></i> <!-- Atau ikon lain seperti fas fa-user -->
+        {{ auth()->user()->name }}
+      </a>
+      <ul class="dropdown-menu dropdown-menu-end bg-dark" aria-labelledby="navbarUserDropdown">
+        <li>
+          <form method="POST" action="{{ route('logout') }}" class="d-inline w-100">
+            @csrf
+            <button type="submit" class="dropdown-item text-light bg-dark">
+              <i class="bi bi-box-arrow-right me-2"></i>Logout
+            </button>
+          </form>
+        </li>
+        <!-- Tambahkan item dropdown lain di sini jika perlu -->
+      </ul>
+    </div>
   @else
     <!-- Tombol Sign In -->
     <a href="{{ route('login') }}" class="btn btn-outline-light me-2">Sign In</a>
